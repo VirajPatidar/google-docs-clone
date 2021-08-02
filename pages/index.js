d33495fb436bf4fc880a3296b07088fd4375061c
Viraj Patidar <vrj381@gmail.com>
2021-08-02 20:10:01 +0530

    document section header

--- pages/index.js
+++ pages/index.js
@@ -37,6 +37,15 @@ export default function Home() {
                     </div>
                 </div>
             </section>
+            <section className="bg-white px-10 md:px-0">
+                <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
+                    <div className="flex items-center justify-between pb-5">
+                        <h2 className="font-medium flex-grow">My Documents</h2>
+                        <h2 className="mr-12">Date Created</h2>
+                        <Icon name="folder" size="3xl" color="gray" />
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
