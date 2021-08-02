8eb57b620a0a498d7c5c7fdc28e40bc583deb101
Viraj Patidar <vrj381@gmail.com>
2021-08-02 19:59:39 +0530

    new doc section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
+import Image from 'next/image'
+import add from '../public/add.png'
 
 export default function Home() {
     return (
@@ -9,7 +13,30 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <Header/>
+            <Header />
+
+            <section className="bg-[#F8F9FA] pb-10 px-10">
+                <div className="max-w-3xl mx-auto">
+                    <div className="flex items-center justify-between py-6">
+                        <h2 className="text-gray-700 text-lg">Start a new document</h2>
+                        <Button
+                            color="gray"
+                            buttonType="outline"
+                            rounded={true}
+                            ripple="dark"
+                            className="border-0"
+                        >
+                            <Icon name="more_vert" size="3xl" />
+                        </Button>
+                    </div>
+                    <div>
+                        <div className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700">
+                            <Image src={add} layout="fill" />
+                        </div>
+                        <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">Blank</p>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
