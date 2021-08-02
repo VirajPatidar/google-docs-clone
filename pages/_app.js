383b4970d39d7304706bfacb592f80f2705c92be
Viraj Patidar <vrj381@gmail.com>
2021-08-02 10:57:51 +0530

    initial setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import 'tailwindcss/tailwind.css'
+import "@material-tailwind/react/tailwind.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <link
+                    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                    rel="stylesheet"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
