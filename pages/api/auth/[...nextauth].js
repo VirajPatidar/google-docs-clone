047f4d962260c769608d30c1a49ef9f3b2fd91df
Viraj Patidar <vrj381@gmail.com>
2021-08-04 12:13:36 +0530

    next-auth setup

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,16 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+import {FirebaseAdapter} from '@next-auth/firebase-adapter'
+import {db} from '../../../firebase'
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+    // ...add more providers here
+  ],
+  adapter: FirebaseAdapter(db)
+})
