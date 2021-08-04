b11d3260082a0c548c357833a56b44ae3f3340e0
Viraj Patidar <vrj381@gmail.com>
2021-08-04 21:52:45 +0530

    about button

--- components/Login.js
+++ components/Login.js
@@ -2,8 +2,12 @@ import Button from "@material-tailwind/react/Button";
 import Image from 'next/image';
 import { signIn } from "next-auth/client";
 import logo from '../public/logo.png'
+import { useRouter } from 'next/dist/client/router';
 
 const Login = () => {
+
+    const router = useRouter();
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <Image
@@ -20,6 +24,15 @@ const Login = () => {
                 onClick={signIn}
             >
                 Login
+            </Button>
+            <Button
+                className="w-54 mt-3"
+                color="teal"
+                buttonType="filled"
+                ripple="light"
+                onClick={() => router.push(`/about`)}
+            >
+                About this Project
             </Button>
         </div>
     );
